9e5fbfe5dd4cade3ee1c6e4ef1f3d7650db19468
sobrock12 <59989171+sobrock12@users.noreply.github.com>
2020-03-19 00:41:01 -0500

    Update database.php

--- Week 8 Midterm/zippyAdmin/model/database.php
+++ Week 8 Midterm/zippyAdmin/model/database.php
@@ -1,9 +1,10 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
-    $username = 'root';
+    $dsn = 'mysql:host=qbhol6k6vexd5qjs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=y5wvftaqzfik8fcu';
+    $username = 'f7vcx3gkmqihlouf';
+    $password = 'fqfappo0bfpmamx9';
 
     try {
-        $db = new PDO($dsn, $username);
+        $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
         $error_message = $e->getMessage();
         include('../errors/database_error.php');
