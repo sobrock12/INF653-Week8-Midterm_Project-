0faf17c8ad1fe8a643707060f0714b056312c09b
sobrock12 <59989171+sobrock12@users.noreply.github.com>
2020-03-19 20:37:28 -0500

    Update index.php

--- Week 8 Midterm/zippy/index.php
+++ Week 8 Midterm/zippy/index.php
@@ -1,10 +1,17 @@
+//index.php is the main controller page for the app. it gets an action from other pages with forms via a $_GET or a $_POST
+//function and then passes variables to other php scripts, which then perform actions like running SQL queries for viewing
+//adding, deleting, and displaying data from the database tables
+
 <?php
-    require('model/database.php');
-    require('model/vehicle_db.php');
-    require('model/type_db.php');
-    require('model/class_db.php');
+    require('model/database.php');              //provides access to database with vehicle tables
+    require('model/vehicle_db.php');            //contains vehicle database functions
+    require('model/type_db.php');               //contains functions for manipulating types
+    require('model/class_db.php');              //contains functions for manipulating classes
 
 
+//checks to see if variable $action was set from a different page of the app via a POST or GET command, sets $action to null
+//if no $action is found. then an if statement checks if $action is null, if it is it runs the if statement under the 'list_vehicles'
+//action
     $action = filter_input(INPUT_POST, 'action');
     if ($action == NULL) {
         $action = filter_input(INPUT_GET, 'action');
@@ -13,6 +20,10 @@
         }
     }
 
+
+//default action. when page is loaded, this action is ran, showing all vehilces info contained in the database
+//also handles what happens when user selects different sort options. receives variables and runs the sort and display
+//function get_vehicles_by_criteria. after this is ran, the vehicle_list.php page is shown.
     if ($action == 'list_vehicles') {
         $all_vehicles = get_all_vehicles();
         $make_selection = isset($_GET['make_selection']) ? $_GET['make_selection'] : '0';
