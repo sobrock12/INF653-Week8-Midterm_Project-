3ca4a0555e5787158cfb99f80b902dbfba2245c8
sobrock12 <59989171+sobrock12@users.noreply.github.com>
2020-03-19 21:56:25 -0500

    Update database.php

--- Week 8 Midterm/zippy/model/database.php
+++ Week 8 Midterm/zippy/model/database.php
@@ -1,4 +1,5 @@
-<?php
+<?php     
+//this connects the web app and the database, initializes a new PDO called $db that connects to the SQL database
     $dsn = 'mysql:host=qbhol6k6vexd5qjs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=y5wvftaqzfik8fcu';
     $username = 'f7vcx3gkmqihlouf';
     $password = 'fqfappo0bfpmamx9';
