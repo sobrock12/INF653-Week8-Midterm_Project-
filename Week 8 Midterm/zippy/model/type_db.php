277f5db6e8cfc3e803a07a3c74f26ece8508380f
sobrock12 <59989171+sobrock12@users.noreply.github.com>
2020-03-19 21:57:22 -0500

    Update type_db.php

--- Week 8 Midterm/zippy/model/type_db.php
+++ Week 8 Midterm/zippy/model/type_db.php
@@ -1,4 +1,5 @@
 <?php
+//function that gets type assigned to vehicle in database, and returns all types as an array $types
     function get_type() {
         global $db;
         $query = 'SELECT * FROM vehicle_type ';
