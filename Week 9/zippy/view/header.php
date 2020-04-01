31990fa08b968bc6017807470d7df05e8641c6a0
Stanley Obrock <sobrock12@gmail.com>
2020-03-31 23:38:27 -0500

    second commit, simplified file structure to eliminate spaces; edited files so admin functionality works without having to have its own separate directory

---
+++ Week 9/zippy/view/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Zippy Used Autos</title>
+    <link rel="stylesheet" type="text/css" href="view/css/main.css" />
+</head>
+
+<!-- the body section -->
+<body>
+    <header>
+        <h1>--------Zippy Used Autos--------</h1><hr>
+    </header>
+
+<div id="centered">
+    <h2>Looking for a vehicle? Search our inventory here!</h2>
+</div>
